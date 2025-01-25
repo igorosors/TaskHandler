692f78f160f0d845e38d4e4569a2ea100cec7a72
Игорь Стрелюхин <i.strelyuhin@profsoft.pro>
2025-01-25 14:34:57 +0400

    replace state flow to shared flow

--- app/src/main/java/ru/taskhandler/ui/utils/TaskHandler.kt
+++ app/src/main/java/ru/taskhandler/ui/utils/TaskHandler.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import ru.taskhandler.ui.model.Task
 import java.util.UUID
@@ -19,7 +19,8 @@ import java.util.UUID
 class TaskHandler(private val scope: CoroutineScope) {
 
     private val jobs = mutableMapOf<String, Job>()
-    private val taskFlow: MutableStateFlow<List<String>> = MutableStateFlow(emptyList())
+    private val taskFlow: MutableSharedFlow<List<String>> = MutableSharedFlow()
+
     val tasks = mutableStateOf<ImmutableMap<String, Task>>(persistentMapOf())
     val debugMessages = mutableStateOf<ImmutableList<String>>(persistentListOf())
 
